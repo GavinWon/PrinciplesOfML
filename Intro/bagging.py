7c139c38ab678851df4d7116128c5cb050f2c984
Gavin Wong <gavinmwong@gmail.com>
2020-02-29 15:04:45 -0800

    bagging" " " ;q` :q~ asdf

---
+++ Intro/bagging.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 29 11:33:51 2020
+
+@author: gavinwong
+"""
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+from sklearn import datasets ## Get dataset from sklearn
+import sklearn.cross_validation as train_test_split ##deprecated, might need to change later
+import sklearn.metrics as sklm
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import numpy.random as nr
+
+def plot_iris(iris):
+    '''Function to plot iris data by type'''
+    setosa = iris[iris['Species'] == 'setosa']
+    versicolor = iris[iris['Species'] == 'versicolor']
+    virginica = iris[iris['Species'] == 'virginica']
+    fig, ax = plt.subplots(2, 2, figsize=(12,12))
+    x_ax = ['Sepal_Length', 'Sepal_Width']
+    y_ax = ['Petal_Length', 'Petal_Width']
+    for i in range(2):
+        for j in range(2):
+            ax[i,j].scatter(setosa[x_ax[i]], setosa[y_ax[j]], marker = 'x')
+            ax[i,j].scatter(versicolor[x_ax[i]], versicolor[y_ax[j]], marker = 'o')
+            ax[i,j].scatter(virginica[x_ax[i]], virginica[y_ax[j]], marker = '+')
+            ax[i,j].set_xlabel(x_ax[i])
+            ax[i,j].set_ylabel(y_ax[j])
+            
+## Import the dataset from sklearn.datasets
+iris = datasets.load_iris()
+
+## Create a data frame from the dictionary
+species = [iris.target_names[x] for x in iris.target]
+iris = pd.DataFrame(iris['data'], columns = ['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width'])
+iris['Species'] = species
+
+## Plot views of the iris data            
+plot_iris(iris) 
+
+Features = np.array(iris[['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width']])
+
+levels = {'setosa':0, 'versicolor':1, 'virginica':2}
+Labels =  np.array([levels[x] for x in iris['Species']])
+
+## Randomly sample cases to create independent training and test data
+nr.seed(1115)
+indx = range(Features.shape[0])
+indx = train_test_split(indx, test_size = 100)
+X_train = Features[indx[0],:]
+y_train = np.ravel(Labels[indx[0]])
+X_test = Features[indx[1],:]
+y_test = np.ravel(Labels[indx[1]])
+
+scale = preprocessing.StandardScaler()
+scale.fit(X_train)
+X_train = scale.transform(X_train)
+
+nr.seed(444)
+rf_clf = RandomForestClassifier(n_estimators=5)
+rf_clf.fit(X_train, y_train)
+
+def print_metrics_3(labels, scores):
+   
+    conf = sklm.confusion_matrix(labels, scores)
+    print('                 Confusion matrix')
+    print('                 Score Setosa   Score Versicolor    Score Virginica')
+    print('Actual Setosa      %6d' % conf[0,0] + '            %5d' % conf[0,1] + '             %5d' % conf[0,2])
+    print('Actual Versicolor  %6d' % conf[1,0] + '            %5d' % conf[1,1] + '             %5d' % conf[1,2])
+    print('Actual Vriginica   %6d' % conf[2,0] + '            %5d' % conf[2,1] + '             %5d' % conf[2,2])
+    ## Now compute and display the accuracy and metrics
+    print('')
+    print('Accuracy        %0.2f' % sklm.accuracy_score(labels, scores))
+    metrics = sklm.precision_recall_fscore_support(labels, scores)
+    print(' ')
+    print('          Setosa  Versicolor  Virginica')
+    print('Num case   %0.2f' % metrics[3][0] + '     %0.2f' % metrics[3][1] + '      %0.2f' % metrics[3][2])
+    print('Precision   %0.2f' % metrics[0][0] + '      %0.2f' % metrics[0][1] + '       %0.2f' % metrics[0][2])
+    print('Recall      %0.2f' % metrics[1][0] + '      %0.2f' % metrics[1][1] + '       %0.2f' % metrics[1][2])
+    print('F1          %0.2f' % metrics[2][0] + '      %0.2f' % metrics[2][1] + '       %0.2f' % metrics[2][2])
+    
+print_metrics_3(y_test, scores)    
+
+def plot_iris_score(iris, y_test, scores):
+    '''Function to plot iris data by type'''
+    ## Find correctly and incorrectly classified cases
+    true = np.equal(scores, y_test).astype(int)
+    
+    ## Create data frame from the test data
+    iris = pd.DataFrame(iris)
+    levels = {0:'setosa', 1:'versicolor', 2:'virginica'}
+    iris['Species'] = [levels[x] for x in y_test]
+    iris.columns = ['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width', 'Species']
+    
+    ## Set up for the plot
+    fig, ax = plt.subplots(2, 2, figsize=(12,12))
+    markers = ['o', '+']
+    x_ax = ['Sepal_Length', 'Sepal_Width']
+    y_ax = ['Petal_Length', 'Petal_Width']
+    
+    for t in range(2): # loop over correct and incorect classifications
+        setosa = iris[(iris['Species'] == 'setosa') & (true == t)]
+        versicolor = iris[(iris['Species'] == 'versicolor') & (true == t)]
+        virginica = iris[(iris['Species'] == 'virginica') & (true == t)]
+        # loop over all the dimensions
+        for i in range(2):
+            for j in range(2):
+                ax[i,j].scatter(setosa[x_ax[i]], setosa[y_ax[j]], marker = markers[t], color = 'blue')
+                ax[i,j].scatter(versicolor[x_ax[i]], versicolor[y_ax[j]], marker = markers[t], color = 'orange')
+                ax[i,j].scatter(virginica[x_ax[i]], virginica[y_ax[j]], marker = markers[t], color = 'green')
+                ax[i,j].set_xlabel(x_ax[i])
+                ax[i,j].set_ylabel(y_ax[j])
+
+plot_iris_score(X_test, y_test, scores)
+
+nr.seed(444)
+rf_clf = RandomForestClassifier(n_estimators=40)
+rf_clf.fit(X_train, y_train)
+scores = rf_clf.predict(X_test)
+print_metrics_3(y_test, scores) 
+plot_iris_score(X_test, y_test, scores)
+
+## Create reduced feature set
+Features = np.array(iris[['Petal_Length', 'Petal_Width']])
+
+## Randomly sample cases to create independent training and test data
+nr.seed(1115)
+indx = range(Features.shape[0])
+indx = train_test_split(indx, test_size = 100)
+X_train = Features[indx[0],:]
+y_train = np.ravel(Labels[indx[0]])
+X_test = Features[indx[1],:]
+y_test = np.ravel(Labels[indx[1]])
+
+nr.seed(444)
+rf_clf = RandomForestClassifier(n_estimators=40)
+rf_clf.fit(X_train, y_train)
+scores = rf_clf.predict(X_test)
+print_metrics_3(y_test, scores) 
+
+Features = np.array(pd.read_csv('Credit_Features.csv'))
+Labels = np.array(pd.read_csv('Credit_Labels.csv'))
+Labels = Labels.reshape(Labels.shape[0],)
+print(Features.shape)
+print(Labels.shape)
+
+nr.seed(123)
+inside = KFold(n_splits=10, shuffle = True)
+nr.seed(321)
+outside = KFold(n_splits=10, shuffle = True)
+
+## Define the dictionary for the grid search and the model object to search on
+param_grid = {"max_features": [2, 3, 5, 10, 15], "min_samples_leaf":[3, 5, 10, 20]}
+## Define the random forest model
+nr.seed(3456)
+rf_clf = RandomForestClassifier(class_weight = "balanced") # class_weight = {0:0.33, 1:0.67}) 
+
+## Perform the grid search over the parameters
+nr.seed(4455)
+rf_clf = ms.GridSearchCV(estimator = rf_clf, param_grid = param_grid, 
+                      cv = inside, # Use the inside folds
+                      scoring = 'roc_auc',
+                      return_train_score = True)
+rf_clf.fit(Features, Labels)
+print(rf_clf.best_estimator_.max_features)
+print(rf_clf.best_estimator_.min_samples_leaf)
+
+nr.seed(498)
+cv_estimate = ms.cross_val_score(rf_clf, Features, Labels, 
+                                 cv = outside) # Use the outside folds
+
+print('Mean performance metric = %4.3f' % np.mean(cv_estimate))
+print('SDT of the metric       = %4.3f' % np.std(cv_estimate))
+print('Outcomes by cv fold')
+for i, x in enumerate(cv_estimate):
+    print('Fold %2d    %4.3f' % (i+1, x))
+    
+## Randomly sample cases to create independent training and test data
+nr.seed(1115)
+indx = range(Features.shape[0])
+indx = ms.train_test_split(indx, test_size = 300)
+X_train = Features[indx[0],:]
+y_train = np.ravel(Labels[indx[0]])
+X_test = Features[indx[1],:]
+y_test = np.ravel(Labels[indx[1]])
+
+nr.seed(1115)
+rf_mod = RandomForestClassifier(class_weight = "balanced", 
+                                max_features = rf_clf.best_estimator_.max_features, 
+                                min_samples_leaf = rf_clf.best_estimator_.min_samples_leaf) 
+rf_mod.fit(X_train, y_train)
+
